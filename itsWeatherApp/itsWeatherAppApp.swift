df389abe5a1e451a02079a8f2ea1b29d9b5dd695
Rafal Pawelec (PL) <rafal.pawelec@pwc.com>
2023-03-16 19:32:53 +0100

    Initial Commit

---
+++ itsWeatherApp/itsWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  itsWeatherAppApp.swift
+//  itsWeatherApp
+//
+//  Created by Rafal Pawelec on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct itsWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
