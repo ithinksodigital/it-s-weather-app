720e28798202db20b90c4ba3f3763f3edc0d0f2f
Rafal Pawelec <pawelec.rafal@gmail.com>
2023-07-07 14:11:39 +0200

    first commit

--- itsWeatherApp/Managers/WeatherManager.swift
+++ itsWeatherApp/Managers/WeatherManager.swift
@@ -10,7 +10,7 @@ import CoreLocation
 
 class WeatherManager {
     func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws -> ResponseBody {
-        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=02aff06bcffaf8a386e26ccc7ca858cd&units=metric") else { fatalError("Missing URL")}
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid={apiKey}&units=metric") else { fatalError("Missing URL")}
         
         let urlRequest = URLRequest(url: url)
         let (data, response) = try await URLSession.shared.data(for: urlRequest)
